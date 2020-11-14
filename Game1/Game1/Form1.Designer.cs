094b1ed8cf98bf684056703d234b7b088f50cc41
HAMIDAT IBRAHIM <gongotso-33d@live.fr>
2020-11-14 16:58:18 +0100

    Add files via upload

---
+++ Game1/Game1/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Game1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.exit_btn = new System.Windows.Forms.PictureBox();
+            this.minimize_btn = new System.Windows.Forms.PictureBox();
+            this.info_btn = new System.Windows.Forms.PictureBox();
+            this.refresh_btn = new System.Windows.Forms.PictureBox();
+            this.check_btn = new System.Windows.Forms.PictureBox();
+            this.hint_31 = new System.Windows.Forms.Label();
+            this.hint_32 = new System.Windows.Forms.Label();
+            this.hint_33 = new System.Windows.Forms.Label();
+            this.hint_21 = new System.Windows.Forms.Label();
+            this.hint_22 = new System.Windows.Forms.Label();
+            this.hint_23 = new System.Windows.Forms.Label();
+            this.hint_11 = new System.Windows.Forms.Label();
+            this.hint_12 = new System.Windows.Forms.Label();
+            this.hint_13 = new System.Windows.Forms.Label();
+            this.hint_41 = new System.Windows.Forms.Label();
+            this.hint_42 = new System.Windows.Forms.Label();
+            this.hint_43 = new System.Windows.Forms.Label();
+            this.txtbx_X3 = new System.Windows.Forms.TextBox();
+            this.txtbx_X2 = new System.Windows.Forms.TextBox();
+            this.txtbx_X1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.exit_btn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minimize_btn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.info_btn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.refresh_btn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.check_btn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.BackColor = System.Drawing.Color.Transparent;
+            this.exit_btn.Location = new System.Drawing.Point(892, 10);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(25, 28);
+            this.exit_btn.TabIndex = 0;
+            this.exit_btn.TabStop = false;
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_Click);
+            // 
+            // minimize_btn
+            // 
+            this.minimize_btn.BackColor = System.Drawing.Color.Transparent;
+            this.minimize_btn.Location = new System.Drawing.Point(859, 10);
+            this.minimize_btn.Name = "minimize_btn";
+            this.minimize_btn.Size = new System.Drawing.Size(25, 28);
+            this.minimize_btn.TabIndex = 1;
+            this.minimize_btn.TabStop = false;
+            this.minimize_btn.Click += new System.EventHandler(this.minimize_btn_Click);
+            // 
+            // info_btn
+            // 
+            this.info_btn.BackColor = System.Drawing.Color.Transparent;
+            this.info_btn.Location = new System.Drawing.Point(827, 10);
+            this.info_btn.Name = "info_btn";
+            this.info_btn.Size = new System.Drawing.Size(25, 28);
+            this.info_btn.TabIndex = 1;
+            this.info_btn.TabStop = false;
+            this.info_btn.Click += new System.EventHandler(this.info_btn_Click);
+            // 
+            // refresh_btn
+            // 
+            this.refresh_btn.BackColor = System.Drawing.Color.Transparent;
+            this.refresh_btn.Location = new System.Drawing.Point(792, 489);
+            this.refresh_btn.Name = "refresh_btn";
+            this.refresh_btn.Size = new System.Drawing.Size(112, 32);
+            this.refresh_btn.TabIndex = 1;
+            this.refresh_btn.TabStop = false;
+            this.refresh_btn.Tag = "4";
+            this.refresh_btn.Click += new System.EventHandler(this.refresh_btn_Click);
+            // 
+            // check_btn
+            // 
+            this.check_btn.AccessibleName = "";
+            this.check_btn.BackColor = System.Drawing.Color.Transparent;
+            this.check_btn.Location = new System.Drawing.Point(397, 218);
+            this.check_btn.Name = "check_btn";
+            this.check_btn.Size = new System.Drawing.Size(112, 32);
+            this.check_btn.TabIndex = 1;
+            this.check_btn.TabStop = false;
+            this.check_btn.Tag = "3";
+            this.check_btn.Click += new System.EventHandler(this.check_btn_Click);
+            // 
+            // hint_31
+            // 
+            this.hint_31.AutoSize = true;
+            this.hint_31.BackColor = System.Drawing.Color.Transparent;
+            this.hint_31.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_31.Location = new System.Drawing.Point(596, 350);
+            this.hint_31.Name = "hint_31";
+            this.hint_31.Size = new System.Drawing.Size(0, 40);
+            this.hint_31.TabIndex = 2;
+            // 
+            // hint_32
+            // 
+            this.hint_32.AutoSize = true;
+            this.hint_32.BackColor = System.Drawing.Color.Transparent;
+            this.hint_32.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_32.Location = new System.Drawing.Point(655, 350);
+            this.hint_32.Name = "hint_32";
+            this.hint_32.Size = new System.Drawing.Size(0, 40);
+            this.hint_32.TabIndex = 2;
+            // 
+            // hint_33
+            // 
+            this.hint_33.AutoSize = true;
+            this.hint_33.BackColor = System.Drawing.Color.Transparent;
+            this.hint_33.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_33.Location = new System.Drawing.Point(716, 350);
+            this.hint_33.Name = "hint_33";
+            this.hint_33.Size = new System.Drawing.Size(0, 40);
+            this.hint_33.TabIndex = 2;
+            // 
+            // hint_21
+            // 
+            this.hint_21.AutoSize = true;
+            this.hint_21.BackColor = System.Drawing.Color.Transparent;
+            this.hint_21.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_21.Location = new System.Drawing.Point(377, 350);
+            this.hint_21.Name = "hint_21";
+            this.hint_21.Size = new System.Drawing.Size(0, 40);
+            this.hint_21.TabIndex = 2;
+            // 
+            // hint_22
+            // 
+            this.hint_22.AutoSize = true;
+            this.hint_22.BackColor = System.Drawing.Color.Transparent;
+            this.hint_22.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_22.Location = new System.Drawing.Point(436, 350);
+            this.hint_22.Name = "hint_22";
+            this.hint_22.Size = new System.Drawing.Size(0, 40);
+            this.hint_22.TabIndex = 2;
+            // 
+            // hint_23
+            // 
+            this.hint_23.AutoSize = true;
+            this.hint_23.BackColor = System.Drawing.Color.Transparent;
+            this.hint_23.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_23.Location = new System.Drawing.Point(496, 350);
+            this.hint_23.Name = "hint_23";
+            this.hint_23.Size = new System.Drawing.Size(0, 40);
+            this.hint_23.TabIndex = 2;
+            // 
+            // hint_11
+            // 
+            this.hint_11.AutoSize = true;
+            this.hint_11.BackColor = System.Drawing.Color.Transparent;
+            this.hint_11.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_11.Location = new System.Drawing.Point(155, 350);
+            this.hint_11.Name = "hint_11";
+            this.hint_11.Size = new System.Drawing.Size(0, 40);
+            this.hint_11.TabIndex = 2;
+            // 
+            // hint_12
+            // 
+            this.hint_12.AutoSize = true;
+            this.hint_12.BackColor = System.Drawing.Color.Transparent;
+            this.hint_12.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_12.Location = new System.Drawing.Point(214, 350);
+            this.hint_12.Name = "hint_12";
+            this.hint_12.Size = new System.Drawing.Size(0, 40);
+            this.hint_12.TabIndex = 2;
+            // 
+            // hint_13
+            // 
+            this.hint_13.AutoSize = true;
+            this.hint_13.BackColor = System.Drawing.Color.Transparent;
+            this.hint_13.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_13.Location = new System.Drawing.Point(276, 350);
+            this.hint_13.Name = "hint_13";
+            this.hint_13.Size = new System.Drawing.Size(0, 40);
+            this.hint_13.TabIndex = 2;
+            // 
+            // hint_41
+            // 
+            this.hint_41.AutoSize = true;
+            this.hint_41.BackColor = System.Drawing.Color.Transparent;
+            this.hint_41.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_41.Location = new System.Drawing.Point(377, 456);
+            this.hint_41.Name = "hint_41";
+            this.hint_41.Size = new System.Drawing.Size(0, 40);
+            this.hint_41.TabIndex = 2;
+            // 
+            // hint_42
+            // 
+            this.hint_42.AutoSize = true;
+            this.hint_42.BackColor = System.Drawing.Color.Transparent;
+            this.hint_42.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_42.Location = new System.Drawing.Point(436, 456);
+            this.hint_42.Name = "hint_42";
+            this.hint_42.Size = new System.Drawing.Size(0, 40);
+            this.hint_42.TabIndex = 2;
+            // 
+            // hint_43
+            // 
+            this.hint_43.AutoSize = true;
+            this.hint_43.BackColor = System.Drawing.Color.Transparent;
+            this.hint_43.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_43.Location = new System.Drawing.Point(497, 456);
+            this.hint_43.Name = "hint_43";
+            this.hint_43.Size = new System.Drawing.Size(0, 40);
+            this.hint_43.TabIndex = 2;
+            // 
+            // txtbx_X3
+            // 
+            this.txtbx_X3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(220)))), ((int)(((byte)(206)))));
+            this.txtbx_X3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtbx_X3.Font = new System.Drawing.Font("Times New Roman", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_X3.Location = new System.Drawing.Point(490, 162);
+            this.txtbx_X3.MaxLength = 1;
+            this.txtbx_X3.Name = "txtbx_X3";
+            this.txtbx_X3.Size = new System.Drawing.Size(49, 36);
+            this.txtbx_X3.TabIndex = 3;
+            this.txtbx_X3.Tag = "2";
+            this.txtbx_X3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtbx_X2
+            // 
+            this.txtbx_X2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(220)))), ((int)(((byte)(206)))));
+            this.txtbx_X2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtbx_X2.Font = new System.Drawing.Font("Times New Roman", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_X2.Location = new System.Drawing.Point(427, 162);
+            this.txtbx_X2.MaxLength = 1;
+            this.txtbx_X2.Name = "txtbx_X2";
+            this.txtbx_X2.Size = new System.Drawing.Size(49, 36);
+            this.txtbx_X2.TabIndex = 3;
+            this.txtbx_X2.Tag = "1";
+            this.txtbx_X2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtbx_X1
+            // 
+            this.txtbx_X1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(220)))), ((int)(((byte)(206)))));
+            this.txtbx_X1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtbx_X1.Font = new System.Drawing.Font("Times New Roman", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_X1.Location = new System.Drawing.Point(364, 162);
+            this.txtbx_X1.MaxLength = 1;
+            this.txtbx_X1.Name = "txtbx_X1";
+            this.txtbx_X1.Size = new System.Drawing.Size(49, 36);
+            this.txtbx_X1.TabIndex = 3;
+            this.txtbx_X1.Tag = "0";
+            this.txtbx_X1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(928, 542);
+            this.Controls.Add(this.txtbx_X1);
+            this.Controls.Add(this.txtbx_X2);
+            this.Controls.Add(this.txtbx_X3);
+            this.Controls.Add(this.hint_13);
+            this.Controls.Add(this.hint_43);
+            this.Controls.Add(this.hint_23);
+            this.Controls.Add(this.hint_33);
+            this.Controls.Add(this.hint_12);
+            this.Controls.Add(this.hint_11);
+            this.Controls.Add(this.hint_42);
+            this.Controls.Add(this.hint_41);
+            this.Controls.Add(this.hint_22);
+            this.Controls.Add(this.hint_21);
+            this.Controls.Add(this.hint_32);
+            this.Controls.Add(this.hint_31);
+            this.Controls.Add(this.check_btn);
+            this.Controls.Add(this.refresh_btn);
+            this.Controls.Add(this.info_btn);
+            this.Controls.Add(this.minimize_btn);
+            this.Controls.Add(this.exit_btn);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.exit_btn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minimize_btn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.info_btn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.refresh_btn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.check_btn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox exit_btn;
+        private System.Windows.Forms.PictureBox minimize_btn;
+        private System.Windows.Forms.PictureBox info_btn;
+        private System.Windows.Forms.PictureBox refresh_btn;
+        private System.Windows.Forms.PictureBox check_btn;
+        private System.Windows.Forms.Label hint_31;
+        private System.Windows.Forms.Label hint_32;
+        private System.Windows.Forms.Label hint_33;
+        private System.Windows.Forms.Label hint_21;
+        private System.Windows.Forms.Label hint_22;
+        private System.Windows.Forms.Label hint_23;
+        private System.Windows.Forms.Label hint_11;
+        private System.Windows.Forms.Label hint_12;
+        private System.Windows.Forms.Label hint_13;
+        private System.Windows.Forms.Label hint_41;
+        private System.Windows.Forms.Label hint_42;
+        private System.Windows.Forms.Label hint_43;
+        private System.Windows.Forms.TextBox txtbx_X3;
+        private System.Windows.Forms.TextBox txtbx_X2;
+        private System.Windows.Forms.TextBox txtbx_X1;
+    }
+}
+
---
+++ Game1/Game1/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game1
+{
+    public partial class Form1 : Form
+    {
+        public static Random rnd = new Random();
+
+        /// Random numbers generator
+        static int RN(int n)
+        { return rnd.Next(n); }
+
+        int X1, X2, X3;
+        int first_ch;
+        int second_ch;
+        int third_ch;
+
+        int[,] P1, P2, P3;
+        int[] P4;
+
+        int[] chosedNumber = new int[7];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // this three varialbles is for storing the correct code
+            initialization();
+
+            // this array here is for storing the rest of numbers except the correct ones
+            rest_nmbrs();
+
+            // and this is for all the possibiltie that could be made in this game
+            possibilities();
+            
+            // all the code below is for dicisions that could be made by the program
+            decisions();
+        }
+
+        private void exit_btn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void minimize_btn_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void refresh_btn_Click(object sender, EventArgs e)
+        {
+            // this three varialbles is for storing the correct code
+            initialization();
+
+            // this array here is for storing the rest of numbers except the correct ones
+            rest_nmbrs();
+
+            // and this is for all the possibiltie that could be made in this game
+            possibilities();
+
+            // all the code below is for dicisions that could be made by the program
+            decisions();
+
+            txtbx_X1.Clear();
+            txtbx_X2.Clear();
+            txtbx_X3.Clear();
+
+            txtbx_X1.Focus();
+        }
+
+        private void info_btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You have to figure out the code from the given hints ");
+        }
+
+        private void check_btn_Click(object sender, EventArgs e)
+        {
+            if (txtbx_X1.Text == Convert.ToString(X1))
+            {
+                if (txtbx_X2.Text == Convert.ToString(X2))
+                {
+                    if (txtbx_X3.Text == Convert.ToString(X3))
+                    {
+                        MessageBox.Show("Congratulation, Your answer is correct!");
+                    }
+                    else
+                        MessageBox.Show("You're wrong try again");
+                }
+                else
+                    MessageBox.Show("You're wrong try again");
+            }
+            else
+                MessageBox.Show("You're wrong try again");
+        }
+
+        /// the three events below is to move the window form
+
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+            }
+        }
+
+        private void rest_nmbrs()
+        {
+            int j = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == X1 || i == X2 || i == X3)
+                    continue;
+                else
+                {
+                    chosedNumber[j] = i;
+                    j++;
+                    if (j == 7) break;
+                }
+            }
+        }
+
+        private void initialization()
+        {
+            X1 = rnd.Next(9);
+            X2 = rnd.Next(9);
+            X3 = rnd.Next(9);
+        }
+
+        private void possibilities()
+        {
+            P1 = new int[,] {
+                { chosedNumber[RN(7)], chosedNumber[RN(7)], X1 },
+                         { chosedNumber[RN(7)], X1, chosedNumber[RN(7)]},
+                         { chosedNumber[RN(7)], chosedNumber[RN(7)], X2},
+                         { X2, chosedNumber[RN(7)], chosedNumber[RN(7)]},
+                         { chosedNumber[RN(7)], X3, chosedNumber[RN(7)]},
+                         { X3, chosedNumber[RN(7)], chosedNumber[RN(7)]}
+            };
+
+            P2 = new int[,] {
+                { X1, chosedNumber[RN(7)], chosedNumber[RN(7)] },
+                           { chosedNumber[RN(7)], X2, chosedNumber[RN(7)] },
+                           { chosedNumber[RN(7)], chosedNumber[RN(7)], X3 }
+            };
+
+            P3 = new int[,] {
+                { chosedNumber[RN(7)], X3, X1 },
+                           { X3, chosedNumber[RN(7)], X1 },
+                           { chosedNumber[RN(7)], X1, X2 },
+                           { X2, X1, chosedNumber[RN(7)] },
+                           { chosedNumber[RN(7)], X3, X2 },
+                           { X3, chosedNumber[RN(7)], X2 }, };
+
+            P4 = new int[] { chosedNumber[RN(7)], chosedNumber[RN(7)], chosedNumber[RN(7)] };
+            while (P4[0] == P4[1] || P4[0] == P4[2] || P4[1] == P4[2])
+            {
+                P4[0] = chosedNumber[RN(7)];
+                P4[1] = chosedNumber[RN(7)];
+                P4[2] = chosedNumber[RN(7)];
+            }
+        }
+
+        private void decisions()
+        {
+            int[][] Groups = new int[6][];
+
+            Groups[0] = new int[] { 0, 1 };
+            Groups[1] = new int[] { 2, 3 };
+            Groups[2] = new int[] { 4, 5 };
+            Groups[3] = new int[] { 0, 1, 2, 3 };
+            Groups[4] = new int[] { 0, 1, 4, 5 };
+            Groups[5] = new int[] { 2, 3, 4, 5 };
+
+            first_ch = rnd.Next(5);
+            second_ch = rnd.Next(3);
+            third_ch = 0;
+
+            if ((first_ch == 0 || first_ch == 1) && second_ch == 0)
+                third_ch = Groups[2][rnd.Next(Groups[2].Length - 1)];
+
+            if ((first_ch == 5 || first_ch == 4) && second_ch == 2)
+                third_ch = Groups[1][rnd.Next(Groups[1].Length - 1)];
+
+            if ((first_ch == 2 || first_ch == 3) && second_ch == 1)
+                third_ch = Groups[0][rnd.Next(Groups[0].Length - 1)];
+
+            if (((first_ch == 0 || first_ch == 1) && second_ch == 1) || ((first_ch == 2 || first_ch == 3) && second_ch == 0))
+                third_ch = Groups[4][rnd.Next(Groups[4].Length - 1)];
+
+            if (((first_ch == 0 || first_ch == 1) && second_ch == 2) || ((first_ch == 4 || first_ch == 5) && second_ch == 0))
+                third_ch = Groups[5][rnd.Next(Groups[5].Length - 1)];
+
+            if (((first_ch == 4 || first_ch == 5) && second_ch == 1) || ((first_ch == 2 || first_ch == 3) && second_ch == 2))
+                third_ch = Groups[3][rnd.Next(Groups[3].Length - 1)];
+
+            hint_11.Text = Convert.ToString(P1[first_ch, 0]);
+            hint_12.Text = Convert.ToString(P1[first_ch, 1]);
+            hint_13.Text = Convert.ToString(P1[first_ch, 2]);
+
+            hint_21.Text = Convert.ToString(P2[second_ch, 0]);
+            hint_22.Text = Convert.ToString(P2[second_ch, 1]);
+            hint_23.Text = Convert.ToString(P2[second_ch, 2]);
+
+            hint_31.Text = Convert.ToString(P3[third_ch, 0]);
+            hint_32.Text = Convert.ToString(P3[third_ch, 1]);
+            hint_33.Text = Convert.ToString(P3[third_ch, 2]);
+
+            hint_41.Text = Convert.ToString(P4[0]);
+            hint_42.Text = Convert.ToString(P4[1]);
+            hint_43.Text = Convert.ToString(P4[2]);
+        }
+    }
+}
